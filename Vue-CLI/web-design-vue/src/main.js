d1f14ed6a45adfd98486fe7673bbe91ae4ce1e06
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-26 15:20:02 +0545

    Main JS

--- Vue-CLI/web-design-vue/src/main.js
+++ Vue-CLI/web-design-vue/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import FriendContact from './components/FriendContact.vue'
-import InputBox from './components/InputBox.vue'
+
 
 const app = createApp(App);
 app.component('friend-contact',FriendContact)
-app.component('input-box',InputBox)
+// app.component('input-box',InputBox)
 app.mount('#app')
 
