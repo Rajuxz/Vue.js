9749e2de9dd4e0a692eb8602dad4d04a3643c721
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-30 21:33:30 +0545

    Adding form to add friends

--- Vue-CLI/web-design-vue/src/components/AddFriend.vue
+++ Vue-CLI/web-design-vue/src/components/AddFriend.vue
@@ -1,6 +1,23 @@
 <template>
     <section>
-        <h1>Hello</h1>
+        <form action="#">
+
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email">
+        </div>
+        <div>
+            <label for="phone">Phone</label>
+            <input type="tel" id="phone">
+        </div>
+        <div>
+            <button>Add Contact</button>
+        </div>
+        </form>
     </section>
 </template>
 
