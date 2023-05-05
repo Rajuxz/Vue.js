ef3c38624d360c54c6fe188f88f9d55c1991c3b1
Rajuxz <Rajankhanalxz@gmail.com>
2023-05-05 15:53:08 +0545

    UserInfo.vue

---
+++ component/src/components/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <div class="top">
+            <span>John Doe</span>
+            <button>Admin</button>
+        </div>
+        <div class="status">
+            <span>Site owner and Admin</span>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            // Do something
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
